1820ea1dab7bbb5e12c6d4cbaf5d7a50ddb8a68c
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-05-30 19:07:46 +0530

    Changed to USER Friendly

--- bot/plugins/media.py
+++ bot/plugins/media.py
@@ -2,36 +2,36 @@ import time
 import asyncio
 import datetime
 
-from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, Filters, InlineKeyboardMarkup, InlineKeyboardButton
 
 from ..utils import is_valid_file, generate_stream_link, get_duration, gen_ik_buttons
-from ..config import Config
-from ..screenshotbot import ScreenShotBot
+from config import Config
+from bot import db, CHAT_FLOOD
 
 
-@ScreenShotBot.on_message(Filters.private & Filters.media)
+@Client.on_message(Filters.private & Filters.media)
 async def _(c, m):
     
     chat_id = m.chat.id
-    if not c.CHAT_FLOOD.get(chat_id):
-        c.CHAT_FLOOD[chat_id] = int(time.time()) - Config.SLOW_SPEED_DELAY-1
+    if not CHAT_FLOOD.get(chat_id):
+        CHAT_FLOOD[chat_id] = int(time.time()) - Config.SLOW_SPEED_DELAY-1
 
-    if int(time.time()) - c.CHAT_FLOOD.get(chat_id) < Config.SLOW_SPEED_DELAY:
+    if int(time.time()) - CHAT_FLOOD.get(chat_id) < Config.SLOW_SPEED_DELAY:
         return
     
-    c.CHAT_FLOOD[chat_id] = int(time.time())
+    CHAT_FLOOD[chat_id] = int(time.time())
     
-    if not await c.db.is_user_exist(chat_id):
-        await c.db.add_user(chat_id)
+    if not await db.is_user_exist(chat_id):
+        await db.add_user(chat_id)
         await c.send_message(
             Config.LOG_CHANNEL,
             f"New User [{m.from_user.first_name}](tg://user?id={chat_id}) started."
         )
     
-    ban_status = await c.db.get_ban_status(chat_id)
+    ban_status = await db.get_ban_status(chat_id)
     if ban_status['is_banned']:
         if (datetime.date.today() - datetime.date.fromisoformat(ban_status['banned_on'])).days > ban_status['ban_duration']:
-            await c.db.remove_ban(chat_id)
+            await db.remove_ban(chat_id)
         else:
             await m.reply_text(
                 f"Sorry Dear, You misused me. So you are **Blocked!**.\n\nBlock Reason: __{ban_status['ban_reason']}__",
@@ -44,7 +44,12 @@ async def _(c, m):
     
     snt = await m.reply_text("Hi there, Please wait while I'm getting everything ready to process your request!", quote=True)
     
-    file_link = generate_stream_link(m)
+    file_link = await generate_stream_link(m)
+    if file_link is None:
+        await snt.edit_text("😟 Sorry! I cannot help you right now, I'm having hard time processing the file.")
+        l = await m.forward(Config.LOG_CHANNEL)
+        await l.reply_text(f'Could not create stream link', True)
+        return
     
     duration = await get_duration(file_link)
     if isinstance(duration, str):
