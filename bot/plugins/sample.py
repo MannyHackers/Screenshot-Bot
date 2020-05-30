8203189e0e519652d4201d101686f9757137fa23
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-05-30 19:08:37 +0530

    Changed to USER Friendly

--- bot/plugins/sample.py
+++ bot/plugins/sample.py
@@ -1,11 +1,10 @@
 import asyncio
 
-from pyrogram import Filters
+from pyrogram import Client, Filters
 
 from ..utils import sample_fn
-from ..screenshotbot import ScreenShotBot
 
 
-@ScreenShotBot.on_callback_query(Filters.create(lambda _, query: query.data.startswith('smpl')))
+@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('smpl')))
 async def _(c, m):
     asyncio.create_task(sample_fn(c, m))
