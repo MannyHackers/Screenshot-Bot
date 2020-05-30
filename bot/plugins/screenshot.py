0032b11e6d046c75af4af17ba0081fce9ed2d5ee
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-05-30 19:09:03 +0530

    Changed to USER Friendly

--- bot/plugins/screenshot.py
+++ bot/plugins/screenshot.py
@@ -1,11 +1,10 @@
 import asyncio
 
-from pyrogram import Filters
+from pyrogram import Client, Filters
 
 from ..utils import screenshot_fn
-from ..screenshotbot import ScreenShotBot
 
 
-@ScreenShotBot.on_callback_query(Filters.create(lambda _, query: query.data.startswith('scht')))
+@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('scht')))
 async def _(c, m):
     asyncio.create_task(screenshot_fn(c, m))
